b76e579ada7d425ff7032ee1f47056b586fbe5f2
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-04-27 14:55:04 -0300

    feat(src): :sparkles: finish player run and idle states

--- src/classes/player.js
+++ src/classes/player.js
@@ -1,9 +1,32 @@
-import { Sprite } from "./sprite.js";
+import { AnimatedSprite } from "./animated-sprite.js";
 
 const playerImage = new Image();
 playerImage.src = "assets/player/idle_1.png";
 
+const playerImage2 = new Image();
+playerImage2.src = "assets/player/idle_2.png";
+
+const playerImage3 = new Image();
+playerImage3.src = "assets/player/idle_3.png";
+
+const playerImage4 = new Image();
+playerImage4.src = "assets/player/idle_4.png";
+
+const playerImageRun1 = new Image();
+playerImageRun1.src = "assets/player/run_1.png";
+
+const playerImageRun2 = new Image();
+playerImageRun2.src = "assets/player/run_2.png";
+
+const playerImageRun3 = new Image();
+playerImageRun3.src = "assets/player/run_3.png";
+
+const playerImageRun4 = new Image();
+playerImageRun4.src = "assets/player/run_4.png";
+
 export class Player {
+  isRunning = false;
+
   keys = {
     w: {
       pressed: false,
@@ -22,10 +45,19 @@ export class Player {
   constructor({ context, position }) {
     this.context = context;
     this.position = position;
-    this.sprite = new Sprite({
+    this.sprite = new AnimatedSprite({
       context,
       position,
-      images: [playerImage],
+      actualState: "idle",
+      allStates: {
+        idle: [playerImage, playerImage2, playerImage3, playerImage4],
+        run: [
+          playerImageRun1,
+          playerImageRun2,
+          playerImageRun3,
+          playerImageRun4,
+        ],
+      },
       size: {
         width: 46,
         height: 80,
@@ -48,6 +80,12 @@ export class Player {
 
     if (this.keys.d.pressed) {
       this.position.x += 3;
+    }
+
+    if (this.isRunning) {
+      this.sprite.changeState("run");
+    } else {
+      this.sprite.changeState("idle");
     }
 
     this.sprite.draw();
---
+++ src/main.js
@@ -0,0 +1,69 @@
+import { Player } from "./classes/player.js";
+
+const canvas = document.getElementById("main-canvas");
+const context = canvas.getContext("2d");
+
+const player = new Player({
+  context,
+  position: {
+    x: 10,
+    y: 10,
+  },
+});
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+}
+animate();
+
+window.addEventListener("keydown", (event) => {
+  // eslint-disable-next-line default-case
+  switch (event.key) {
+    case "w":
+      player.isRunning = true;
+      player.keys.w.pressed = true;
+      break;
+
+    case "a":
+      player.isRunning = true;
+      player.keys.a.pressed = true;
+      break;
+
+    case "s":
+      player.isRunning = true;
+      player.keys.s.pressed = true;
+      break;
+
+    case "d":
+      player.isRunning = true;
+      player.keys.d.pressed = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  // eslint-disable-next-line default-case
+  switch (event.key) {
+    case "w":
+      player.isRunning = false;
+      player.keys.w.pressed = false;
+      break;
+
+    case "a":
+      player.isRunning = false;
+      player.keys.a.pressed = false;
+      break;
+
+    case "s":
+      player.isRunning = false;
+      player.keys.s.pressed = false;
+      break;
+
+    case "d":
+      player.isRunning = false;
+      player.keys.d.pressed = false;
+      break;
+  }
+});
