313381c4ee3aa05468e0d2c77320d2b12f352b3b
Matheus Alvarenga <matheus.alvarenga@natahouse.com>
2022-05-01 14:38:26 -0300

    feat(data): :sparkles: add shoot state animation frames

--- src/data/bow.js
+++ src/data/bow.js
@@ -1,8 +1,18 @@
 const bow = new Image();
 bow.src = "assets/bow/idle.png";
 
+const bowShoot1 = new Image();
+bowShoot1.src = "assets/bow/shoot_1.png";
+
+const bowShoot2 = new Image();
+bowShoot2.src = "assets/bow/shoot_2.png";
+
+const bowShoot3 = new Image();
+bowShoot3.src = "assets/bow/shoot_3.png";
+
 export default {
   animationFrames: {
     idle: [bow],
+    shoot: [bowShoot1, bowShoot2, bowShoot3]
   },
 };
