20ce69c27f82b8740619cabeae3f20a1770802d4
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-05-02 15:45:06 -0300

    feat(utils): :sparkles: add new util for calculate projectile velocity

--- src/utils/mouse.js
+++ src/utils/mouse.js
@@ -1,7 +1,16 @@
 export const mouseEvents = (canvas, e) => {
   const bounds = canvas.getBoundingClientRect();
   return {
-    x: e.pageX - bounds.left - scrollX,
-    y: e.pageY - bounds.top - scrollY,
+    x: e.pageX - bounds.left - window.scrollX,
+    y: e.pageY - bounds.top - window.scrollY,
+  };
+};
+
+export const velocityCalculator = (mouse, position) => {
+  const angle = Math.atan2(mouse.y - position.y, mouse.x - position.x);
+
+  return {
+    x: Math.cos(angle) * 10,
+    y: Math.sin(angle) * 10,
   };
 };
