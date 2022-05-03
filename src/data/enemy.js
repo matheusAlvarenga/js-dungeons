9d35e89d121e858f7dcd81efca9566c23419ba24
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-05-03 15:20:57 -0300

    feat(data, assets): :sparkles: add warrior orc idle frames

---
+++ src/data/enemy.js
@@ -0,0 +1,27 @@
+const orcWarriorIdle1 = new Image();
+orcWarriorIdle1.src = "assets/orc/warrior/idle_1.png";
+
+const orcWarriorIdle2 = new Image();
+orcWarriorIdle2.src = "assets/orc/warrior/idle_2.png";
+
+const orcWarriorIdle3 = new Image();
+orcWarriorIdle3.src = "assets/orc/warrior/idle_3.png";
+
+const orcWarriorIdle4 = new Image();
+orcWarriorIdle4.src = "assets/orc/warrior/idle_4.png";
+
+export default {
+  orcs: {
+    warrior: {
+      animationFrames: {
+        idle: [
+          orcWarriorIdle1,
+          orcWarriorIdle2,
+          orcWarriorIdle3,
+          orcWarriorIdle4,
+        ],
+        run: [],
+      },
+    },
+  },
+};
