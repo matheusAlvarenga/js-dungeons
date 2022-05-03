86340c9e87cfa9f7137ad7e89ea52107a3f5e725
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-05-03 18:36:55 -0300

    feat(utils): :sparkles: improve basic collision util

--- src/utils/basic-collision.js
+++ src/utils/basic-collision.js
@@ -1,9 +1,9 @@
 export const detectBasicCollision = (playerHitBox, actorHitBox) => {
   if (
-    playerHitBox.x > actorHitBox.x &&
-    playerHitBox.x + playerHitBox.width < actorHitBox.x + actorHitBox.width &&
-    playerHitBox.y > actorHitBox.y &&
-    playerHitBox.y + playerHitBox.height < actorHitBox.y + actorHitBox.height
+    playerHitBox.x + playerHitBox.width > actorHitBox.x &&
+    playerHitBox.x - playerHitBox.width < actorHitBox.x + actorHitBox.width &&
+    playerHitBox.y + playerHitBox.height > actorHitBox.y &&
+    playerHitBox.y - playerHitBox.height < actorHitBox.y + actorHitBox.height
   )
     return true;
 
