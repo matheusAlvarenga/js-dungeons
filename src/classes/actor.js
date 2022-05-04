79e4c6322ebe89e924d4e1dfd5fc2c88e213d180
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-05-04 15:42:58 -0300

    fix(classes): :bug: fix bug when enemy dies

--- src/classes/actor.js
+++ src/classes/actor.js
@@ -25,7 +25,7 @@ export class Actor extends AnimatedSprite {
     this.actualState = newState;
     this.animationFrames = this.states[newState];
     this.frames.totalFrames = this.animationFrames.length;
-    if (this.frames.totalFrames < this.frames.val) {
+    if (this.frames.totalFrames <= this.frames.val) {
       this.frames.val = 0;
       this.frames.elapsed = 0;
     }
