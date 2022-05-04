f1d4538b31549a1fac77ecbbd91e6f31dece2afc
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-05-04 09:04:53 -0300

    fix(utils): :bug: fix collision

--- src/utils/basic-collision.js
+++ src/utils/basic-collision.js
@@ -1,9 +1,9 @@
 export const detectBasicCollision = (playerHitBox, actorHitBox) => {
   if (
     playerHitBox.x + playerHitBox.width > actorHitBox.x &&
-    playerHitBox.x - playerHitBox.width < actorHitBox.x + actorHitBox.width &&
+    playerHitBox.x < actorHitBox.x + actorHitBox.width &&
     playerHitBox.y + playerHitBox.height > actorHitBox.y &&
-    playerHitBox.y - playerHitBox.height < actorHitBox.y + actorHitBox.height
+    playerHitBox.y < actorHitBox.y + actorHitBox.height
   )
     return true;
 
